deb76e88fac939782dad339c55889e2ff75775fc
Anshaj Vats <9931633906vats@gmail.com>
2024-11-15 23:14:54 -0800

    Remove all files from GitHub repository

--- src/server/routes/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as auth } from "./auth";
-export { default as home } from "./home";
-export { default as mainLobby } from "./main-lobby";
-export { default as games } from "./games";
